3049c98eda33ec5e492a929b62c62f736fe7e10b
Nav Sharma <navdhruv12@gmail.com>
2026-01-16 16:11:28 +0530

    Second Commit

---
+++ Index.js
@@ -0,0 +1,14 @@
+// 1  hello Worls
+console.log("Node.js Backend Started");
+
+//  2 Varibles 
+let name = "Tarun";       //Strig
+let age = 21;             //number
+const country = "India";  //constant
+let isStudent = true;     //boolean
+
+console.log(name);
+console.log(age);
+console.log(country);
+console.log(isStudent);
+
--- Index1.js
+++ Index1.js
@@ -1,13 +1,27 @@
-// 1  hello Worls
-console.log("Node.js Backend Started");
+let str="NAV SHARMA";
+let result=str.toUpperCase();
+let result1=str.toLowerCase();
 
-//  2 Varibles 
-let name = "Tarun";       //Strig
-let age = 21;             //number
-const country = "India";  //constant
-let isStudent = true;     //boolean
+console.log(result1);
+console.log(result);
 
-console.log(name);
-console.log(age);
-console.log(country);
-console.log(isStudent);
+let str1="   Hello World   ";
+console.log(str1.trim());
+include=str1.includes("World");
+console.log(include);
+
+let a="admainogmal.com";
+startwith=a.startsWith("admin");
+endwith=a.endsWith("com");
+console.log(startwith);
+console.log(endwith);
+
+let b="admainogmal.com";
+let part=b.slice(2,7);
+console.log(part);
+
+let c="aabc";
+let rep=c.replace("a","x");
+console.log(rep);
+let rep1=c.replaceAll("a","x");
+console.log(rep1);
