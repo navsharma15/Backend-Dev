bccc4e927c9d825116bd6959ef9efbae13825d4e
Nav Sharma <navdhruv12@gmail.com>
2026-01-16 16:18:41 +0530

    Third Commit

--- Index1.js
+++ Index1.js
@@ -9,19 +9,3 @@ let str1="   Hello World   ";
 console.log(str1.trim());
 include=str1.includes("World");
 console.log(include);
-
-let a="admainogmal.com";
-startwith=a.startsWith("admin");
-endwith=a.endsWith("com");
-console.log(startwith);
-console.log(endwith);
-
-let b="admainogmal.com";
-let part=b.slice(2,7);
-console.log(part);
-
-let c="aabc";
-let rep=c.replace("a","x");
-console.log(rep);
-let rep1=c.replaceAll("a","x");
-console.log(rep1);
