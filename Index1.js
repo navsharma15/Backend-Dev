cda488af7471e2937706e334315992c6be776ef0
Nav Sharma <navdhruv12@gmail.com>
2026-01-16 16:22:00 +0530

    Fourth commit

--- Index1.js
+++ Index1.js
@@ -9,3 +9,9 @@ let str1="   Hello World   ";
 console.log(str1.trim());
 include=str1.includes("World");
 console.log(include);
+
+let a="admingmail.com";
+startwith=a.startsWith("admin");
+endwith=a.endsWith("com");
+console.log(startwith);
+console.log(endwith);
