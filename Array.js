52d97fc2661cc615c5318f6a620057a21e8dc456
Nav Sharma <navdhruv12@gmail.com>
2026-01-19 11:19:21 +0530

    first commit

---
+++ Array.js
@@ -0,0 +1,10 @@
+// Array is a data structure used to store multiple values in a single variable. It is an ordered collection of items, where each item can be accessed by its index. Arrays can hold items of different data types, including numbers, strings, objects, and even other arrays.
+
+// 1️ push() =Add at END
+
+//  Adds element(s) to the end of the array
+
+let arr1 = [1, 2, 3];
+arr1.push(4);
+
+console.log(arr1); 
