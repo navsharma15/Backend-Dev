c867c0d60d972ecb23cdd742c42c79585dd80450
Nav Sharma <navdhruv12@gmail.com>
2026-01-23 14:39:56 +0530

    Solve System Information Logger

---
+++ StringProject/app.js
@@ -0,0 +1,5 @@
+const { capitalize, reverseString, countVowels } = require("./stringUtils");
+
+console.log(capitalize("hello"));        
+console.log(reverseString("world"));     
+console.log(countVowels("education"));   
